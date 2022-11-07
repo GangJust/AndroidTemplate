f4d6ca24da04405ab7ad88e2a350460c2e5f178b
GangJust <201741884@qq.com>
2022-11-07 13:43:06 +0800

    update
    update

--- app/src/main/java/com/freegang/testat/recycler/bean/ItemBean.kt
+++
@@ -1,5 +0,0 @@
-package com.freegang.testat.recycler.bean
-
-import com.freegang.androidtemplate.base.recycler.BaseItem
-
-data class ItemBean(var title: String) : BaseItem("key_" + System.currentTimeMillis())
